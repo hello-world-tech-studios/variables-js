19c66b486828b021551b2bce57c8d07dc98b664c
Sarah Betack <msbetack@gmail.com>
2017-06-09 15:02:14 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# JAVASCRIPT
+* Open `index.html` in chrome
+* Follow the steps on the webpage to open the console in chrome's developer tools
+* with the console and sublime side-by-side, navigate to `script.js` in sublime
---
+++ script.js
@@ -0,0 +1,21 @@
+//The line below says that once everything is loaded in the browser, run the code inside the {}
+document.addEventListener('DOMContentLoaded', function() {
+
+
+var variable = " VARIABLE GOES HERE"
+var happy = ", how are you!? I'm so happy.";
+var sad = ", how are you? I'm having a bad day.";
+var greeting = ", wasssssuppp!?";
+var negativeFive = -5
+var two = 2;
+var eight = 8;
+
+//REPLACE the word variable with one of the varibales above so that the console.log prints out what is on the right of the ==.
+console.log(10 * variable) == -50;
+console.log("Hello" + variable) == "Hello, how are you!? I'm so happy.";
+console.log("Hello" + variable) == "Hello, how are you? I'm having a bad day.";
+console.log(82 - variable) == 80;
+
+});
+
+//Jot down a few thoughts on why you think variables are important in programming below.
