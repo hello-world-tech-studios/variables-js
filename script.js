18817d474df7ef3f6b4378c41755e39e1207a086
Hack Reactor Student <student@hackreactor.com>
2017-08-08 16:03:32 -0500

    zachsvariables

---
+++ script.js
@@ -0,0 +1,48 @@
+//The line below says that once the webpage is loaded in the browser, run the code between the {}
+document.addEventListener('DOMContentLoaded', function() {
+// ______________________________________________________//
+//// We have defined a few javascript variables below////
+// ______________________________________________________//
+var putTheVariableHere = ""
+var happy = ", how are you!? I'm so happy.";
+var sad = ", how are you? I'm having a bad day.";
+var greeting = "Hey, wasssssuppp";
+var negativeFive = -5
+var two = 2;
+var eight = 8;
+var coder = ", I'm a coder!";
+var eighty = 80;
+// ______________________________________________________//
+// ______________________________________________________//
+// REPLACE putTheVariableHere with one of the variables above so that the console.log prints out what is on the right of the ==. Refresh the webpage to make sure you are console logging the correct statement each time.
+// ______________________________________________________//
+// ______________________________________________________//
+ //console.log(10 * putTheVariableHere) == -50;
+ //console.log("Hello" + happy) == "Hello, how are you!? I'm so happy.";
+ //console.log(82 - two) == 80;
+ //console.log(greeting + "!?!?!") == "Hey, wasssssuppp!?!?!";
+ //console.log("Hello" + sad) == "Hello, how are you? I'm having a bad day.";
+//
+//
+// // Create your own varibales to use below.
+//
+//
+//
+//
+// // Use the variables you created to complete the equalities below.
+   //console.log("Hi" + coder) == "Hi,  I'm a coder!";
+   //console.log(eighty / 10) == 8;
+});
+//
+// // ______________________________________________________//
+// // ______________________________________________________//
+// //Jot down a few thoughts on why you think variables are important to programming below.
+// /*
+
+
+
+
+
+
+
+// ______________________________________________________//
