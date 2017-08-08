e3b1a715cbcb8087436be3da34a5d8332b466c2f
Hack Reactor Student <student@hackreactor.com>
2017-08-08 15:43:07 -0500

    lukes variables

--- script.js
+++ script.js
@@ -15,21 +15,21 @@ var eight = 8;
 // REPLACE putTheVariableHere with one of the variables above so that the console.log prints out what is on the right of the ==. Refresh the webpage to make sure you are console logging the correct statement each time.
 // ______________________________________________________//
 // ______________________________________________________//
-console.log(10 * putTheVariableHere) == -50;
-console.log("Hello" + putTheVariableHere) == "Hello, how are you!? I'm so happy.";
-console.log(82 - putTheVariableHere) == 80;
-console.log(putTheVariableHere + "!?!?!") == "Hey, wasssssuppp!?!?!";
-console.log("Hello" + putTheVariableHere) == "Hello, how are you? I'm having a bad day.";
-
-
-// Create your own varibales to use below.
-
-
-
-
-// Use the variables you created to complete the equalities below.
-  console.log("Hi" + putTheVariableHere) == "Hi, I'm a coder!";
-  console.log(putTheVariableHere / 10) == 8;
+console.log(10 * negativeFive) == -50;
+console.log("Hello" + happy) == "Hello, how are you!? I'm so happy.";
+console.log(82 - two) == 80;
+console.log( greeting+ "!?!?!") == "Hey, wasssssuppp!?!?!";
+console.log("Hello" +sad  ) == "Hello, how are you? I'm having a bad day.";
+//
+//
+var qwaa="qween with a appitite"Create your own varibales to use below.
+//
+//
+//
+//
+// // Use the variables you created to complete the equalities below.
+//   console.log("Hi" + putTheVariableHere) == "Hi, I'm a coder!";
+//   console.log(putTheVariableHere / 10) == 8;
 });
 
 // ______________________________________________________//
